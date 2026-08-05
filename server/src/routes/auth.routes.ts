d74153573b4b01be29697565ef751bb99b0ad883
Priyangaj-17 <priyangaj17@gmai.com>
2026-08-05 10:09:05 +0530

    Remove legacy Express server and old client folder

--- server/src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signup, loginUser } from "../controllers/auth.controller";
-
-const router = Router();
-
-router.post("/signup", signup);
-router.post("/login", loginUser);
-
-export default router;
